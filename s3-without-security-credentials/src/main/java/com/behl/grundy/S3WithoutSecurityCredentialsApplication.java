570d91e8be57cee84f7a6db950aabd50c7921950
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-02-14 12:50:31 +0530

    s3-without-sc: init

---
+++ s3-without-security-credentials/src/main/java/com/behl/grundy/S3WithoutSecurityCredentialsApplication.java
@@ -0,0 +1,13 @@
+package com.behl.grundy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3WithoutSecurityCredentialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3WithoutSecurityCredentialsApplication.class, args);
+	}
+
+}
