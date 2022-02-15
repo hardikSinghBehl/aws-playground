ab70f7518a60d8ccba8264b304ac1866aaa6d074
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-02-15 13:37:23 +0530

    s3-presigned-url: init commit

---
+++ s3-presigned-url/src/main/java/com/behl/sampler/S3PresignedUrlApplication.java
@@ -0,0 +1,13 @@
+package com.behl.sampler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3PresignedUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3PresignedUrlApplication.class, args);
+	}
+
+}
