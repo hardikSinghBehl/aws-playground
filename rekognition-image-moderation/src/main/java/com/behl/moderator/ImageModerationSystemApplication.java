6c78cfcdaf265df90e810756da2775b3d9278b1b
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-02-23 08:36:59 +0530

    rekognition-image-moderation: init commit

---
+++ rekognition-image-moderation/src/main/java/com/behl/moderator/ImageModerationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.behl.moderator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageModerationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageModerationSystemApplication.class, args);
+	}
+
+}
