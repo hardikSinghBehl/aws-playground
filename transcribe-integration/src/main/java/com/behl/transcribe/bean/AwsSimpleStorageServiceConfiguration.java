d5191c9748103bf0bcd595439fb646b5c792c29a
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-02-23 13:38:58 +0530

    transcribe: adding s3 dependency and bean

--- transcribe-integration/src/main/java/com/behl/transcribe/bean/AwsSimpleStorageServiceConfiguration.java
+++ transcribe-integration/src/main/java/com/behl/transcribe/bean/AwsSimpleStorageServiceConfiguration.java
@@ -4,11 +4,12 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
-import com.amazonaws.services.transcribe.AmazonTranscribe;
-import com.amazonaws.services.transcribe.AmazonTranscribeClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.behl.transcribe.properties.AwsIAMConfigurationProperties;
 
 import lombok.AllArgsConstructor;
@@ -19,19 +20,19 @@ import lombok.AllArgsConstructor;
  */
 
 @Configuration
-@EnableConfigurationProperties(value = AwsIAMConfigurationProperties.class)
 @AllArgsConstructor
-public class AmazonTranscribeConfiguration {
+@EnableConfigurationProperties(value = AwsIAMConfigurationProperties.class)
+public class AwsSimpleStorageServiceConfiguration {
 
 	private final AwsIAMConfigurationProperties awsIAMConfigurationProperties;
 
 	@Bean
-	public AmazonTranscribe amazonTranscribe() {
-		BasicAWSCredentials credentials = new BasicAWSCredentials(awsIAMConfigurationProperties.getAccessKey(),
+	public AmazonS3 amazonS3() {
+		AWSCredentials awsCredentials = new BasicAWSCredentials(awsIAMConfigurationProperties.getAccessKey(),
 				awsIAMConfigurationProperties.getSecretAccessKey());
 
-		return AmazonTranscribeClientBuilder.standard().withRegion(Regions.AP_SOUTH_1)
-				.withCredentials(new AWSStaticCredentialsProvider(credentials)).build();
+		return AmazonS3ClientBuilder.standard().withRegion(Regions.AP_SOUTH_1)
+				.withCredentials(new AWSStaticCredentialsProvider(awsCredentials)).build();
 	}
 
 }
