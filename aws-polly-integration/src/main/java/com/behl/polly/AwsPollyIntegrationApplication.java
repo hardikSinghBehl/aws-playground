5b999fc4ea3058be141de62012fcb66a98773cb0
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-02-27 21:31:57 +0530

    polly: init commit

---
+++ aws-polly-integration/src/main/java/com/behl/polly/AwsPollyIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.behl.polly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsPollyIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsPollyIntegrationApplication.class, args);
+	}
+
+}
