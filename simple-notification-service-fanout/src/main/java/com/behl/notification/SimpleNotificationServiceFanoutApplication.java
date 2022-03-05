c0c529920785ebcbb6bfb3bf5dc5516a58e5a23d
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-03-05 16:32:42 +0530

    sns-fanout: init commit

---
+++ simple-notification-service-fanout/src/main/java/com/behl/notification/SimpleNotificationServiceFanoutApplication.java
@@ -0,0 +1,13 @@
+package com.behl.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleNotificationServiceFanoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleNotificationServiceFanoutApplication.class, args);
+	}
+
+}
