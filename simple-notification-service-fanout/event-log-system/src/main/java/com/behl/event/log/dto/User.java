01f0fa1b6464d27757b6395dcefd11f499eaf9ed
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-03-05 19:12:16 +0530

    sns-fanout: adding queue-2 consumer - event-log-system

---
+++ simple-notification-service-fanout/event-log-system/src/main/java/com/behl/event/log/dto/User.java
@@ -0,0 +1,15 @@
+package com.behl.event.log.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.extern.jackson.Jacksonized;
+
+@Getter
+@Builder
+@Jacksonized
+public class User {
+
+	private final String emailId;
+	private final String password;
+
+}
