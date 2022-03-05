e11e9d04a8429d5534416c9c4d0e17c9ec70a47c
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-03-05 11:46:41 +0530

    simple-queue-service: init commit

---
+++ simple-queue-service/sqs-consumer/src/main/java/com/behl/sqs/consumer/SqsConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.behl.sqs.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqsConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqsConsumerApplication.class, args);
+	}
+
+}
---
+++ simple-queue-service/sqs-publisher/src/main/java/com/behl/sqs/publisher/SqsPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.behl.sqs.publisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqsPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqsPublisherApplication.class, args);
+	}
+
+}
