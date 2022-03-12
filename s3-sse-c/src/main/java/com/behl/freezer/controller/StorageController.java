3c65f2f7f851dbc890aba16241325ee6713ee303
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-03-12 20:15:16 +0530

    s3-sse-c: ++ API endpoints

---
+++ s3-sse-c/src/main/java/com/behl/freezer/controller/StorageController.java
@@ -0,0 +1,39 @@
+package com.behl.freezer.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.services.s3.model.S3Object;
+import com.behl.freezer.service.StorageService;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(value = "/files")
+public class StorageController {
+
+	private final StorageService storageService;
+
+	@PostMapping
+	@ResponseStatus(value = HttpStatus.OK)
+	public ResponseEntity<HttpStatus> save(@RequestPart(name = "file", required = true) final MultipartFile file) {
+		return ResponseEntity.status(storageService.save(file)).build();
+	}
+
+	@GetMapping(value = "/{key}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(value = HttpStatus.OK)
+	public ResponseEntity<S3Object> retrive(@PathVariable(required = true, name = "key") final String objectKey) {
+		return ResponseEntity.ok(storageService.retrieve(objectKey));
+	}
+
+}
