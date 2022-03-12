dc33cd68bdd59e5d36f0a5b3aef5c7117eae2b1b
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-03-12 19:14:31 +0530

    s3-sse-c: init commit

---
+++ s3-sse-c/src/main/java/com/behl/freezer/S3CustomerManagedKeyEncryptionApplication.java
@@ -0,0 +1,13 @@
+package com.behl.freezer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3CustomerManagedKeyEncryptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3CustomerManagedKeyEncryptionApplication.class, args);
+	}
+
+}
