10f43a05ff51918321c6d57d262264e74de89995
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-03-15 00:53:11 +0530

    sns-fanout: enabling automatic acknowledgment

--- simple-notification-service-fanout/email-notification-system/src/main/java/com/behl/email/notifier/EmailNotificationSystemApplication.java
+++ simple-notification-service-fanout/email-notification-system/src/main/java/com/behl/email/notifier/EmailNotificationSystemApplication.java
@@ -4,38 +4,30 @@ import java.time.LocalDateTime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration;
 import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
 import org.springframework.cloud.aws.messaging.config.annotation.NotificationMessage;
+import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.http.HttpStatus;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.web.server.ResponseStatusException;
 
-import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.behl.email.notifier.dto.User;
-import com.behl.email.notifier.properties.AwsSqsConfigurationProperties;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication(exclude = { ContextRegionProviderAutoConfiguration.class, ContextStackAutoConfiguration.class })
 @Slf4j
-@AllArgsConstructor
-@EnableConfigurationProperties(AwsSqsConfigurationProperties.class)
 public class EmailNotificationSystemApplication {
-
-	private final AmazonSQSAsync amazonSQSAsync;
-	private final AwsSqsConfigurationProperties awsSqsConfigurationProperties;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmailNotificationSystemApplication.class, args);
 	}
 
-	@SqsListener(value = "email-notification-queue")
+	@SqsListener(value = "email-notification-queue", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
 	public void consumer(@NotificationMessage final String message,
 			@Header(name = "ReceiptHandle") final String receiptHandle) {
 		User user;
@@ -47,7 +39,6 @@ public class EmailNotificationSystemApplication {
 		}
 
 		log.info("SENDING ACCOUNT CONFIRMATION EMAIL TO USER {}: {}", user.getEmailId(), LocalDateTime.now());
-		amazonSQSAsync.deleteMessage(awsSqsConfigurationProperties.getUrl(), receiptHandle);
 	}
 
 }
