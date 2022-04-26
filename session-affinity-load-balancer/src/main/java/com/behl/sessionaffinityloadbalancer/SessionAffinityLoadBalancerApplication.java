2d4de763c6320c87ba8b53d82baec27c762ec474
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-04-26 23:06:28 +0530

    session-affinity: init commit

---
+++ session-affinity-load-balancer/src/main/java/com/behl/sessionaffinityloadbalancer/SessionAffinityLoadBalancerApplication.java
@@ -0,0 +1,13 @@
+package com.behl.sessionaffinityloadbalancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionAffinityLoadBalancerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionAffinityLoadBalancerApplication.class, args);
+	}
+
+}
