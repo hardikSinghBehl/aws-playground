5dc884f4f9ff016f0a7c14d8b89c1866835d30cf
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-04-26 21:05:39 +0530

    client-side-encryption: init commit

---
+++ s3-client-side-encryption/src/main/java/com/behl/monjo/S3ClientSideEncryptionApplication.java
@@ -0,0 +1,13 @@
+package com.behl.monjo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3ClientSideEncryptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3ClientSideEncryptionApplication.class, args);
+	}
+
+}
