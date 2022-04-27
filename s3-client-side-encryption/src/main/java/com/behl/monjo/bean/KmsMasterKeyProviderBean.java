726604fbd8681a5a7384c7b2dccfade8c6a0b75a
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-04-27 09:08:49 +0530

    modifying KMS bean to include credentials

--- s3-client-side-encryption/src/main/java/com/behl/monjo/bean/KmsMasterKeyProviderBean.java
+++ s3-client-side-encryption/src/main/java/com/behl/monjo/bean/KmsMasterKeyProviderBean.java
@@ -5,22 +5,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.encryptionsdk.kms.KmsMasterKeyProvider;
+import com.behl.monjo.properties.AwsIAMConfigurationProperties;
 import com.behl.monjo.properties.AwsKmsConfigurationProperties;
 
 import lombok.AllArgsConstructor;
 
 @Configuration
 @AllArgsConstructor
-@EnableConfigurationProperties(value = AwsKmsConfigurationProperties.class)
+@EnableConfigurationProperties(value = { AwsKmsConfigurationProperties.class, AwsIAMConfigurationProperties.class })
 public class KmsMasterKeyProviderBean {
 
 	private final AwsKmsConfigurationProperties awsKmsConfigurationProperties;
+	private final AwsIAMConfigurationProperties awsIAMConfigurationProperties;
 
 	@Bean
 	@Primary
 	public KmsMasterKeyProvider kmsMasterKeyProvider() {
-		return KmsMasterKeyProvider.builder().buildStrict(awsKmsConfigurationProperties.getKms().getKeyArn());
+		AWSCredentials awsCredentials = new BasicAWSCredentials(awsIAMConfigurationProperties.getAccessKey(),
+				awsIAMConfigurationProperties.getSecretAccessKey());
+		return KmsMasterKeyProvider.builder().withDefaultRegion("ap-south-1")
+				.withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+				.buildStrict(awsKmsConfigurationProperties.getKms().getKeyArn());
 	}
 
 }
