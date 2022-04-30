f2f34ee8a4770ef841316e5709dea0b75669daa6
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-04-30 11:40:58 +0530

    sticky-session-lb: init commit

---
+++ sticky-session-load-balancer/src/main/java/com/behl/sticky/StickySessionLoadBalancerApplication.java
@@ -0,0 +1,13 @@
+package com.behl.sticky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StickySessionLoadBalancerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StickySessionLoadBalancerApplication.class, args);
+	}
+
+}
