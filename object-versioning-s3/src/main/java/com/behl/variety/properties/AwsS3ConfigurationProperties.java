f7db56d2f4a2207b24afb6ebc8845f2afe3d2536
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-05-01 21:08:33 +0530

    object-versioning: adding S3 configuration classes

---
+++ object-versioning-s3/src/main/java/com/behl/variety/properties/AwsS3ConfigurationProperties.java
@@ -0,0 +1,18 @@
+package com.behl.variety.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
+
+@Data
+@ConfigurationProperties(prefix = "com.behl.aws")
+public class AwsS3ConfigurationProperties {
+
+	private S3 s3 = new S3();
+
+	@Data
+	public class S3 {
+		private String bucketName;
+	}
+
+}
