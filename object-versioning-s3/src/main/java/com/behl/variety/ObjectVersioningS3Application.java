517c21748bcac740e4f4de1d16904ec7f3ed9a9a
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-05-01 21:02:18 +0530

    object-versioning: init commit

---
+++ object-versioning-s3/src/main/java/com/behl/variety/ObjectVersioningS3Application.java
@@ -0,0 +1,13 @@
+package com.behl.variety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectVersioningS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjectVersioningS3Application.class, args);
+	}
+
+}
