4f0d702cb6643ec0b2173993e83ac24697233d6c
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-05-03 14:29:57 +0530

    multipart-upload-s3: init POC commit

---
+++ multipart-upload-s3/src/main/java/com/behl/chubby/MultipartUploadS3Application.java
@@ -0,0 +1,13 @@
+package com.behl.chubby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipartUploadS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipartUploadS3Application.class, args);
+	}
+
+}
