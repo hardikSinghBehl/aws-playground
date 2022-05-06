3f446e9764478746622a040dac622f2a065b19b0
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-05-06 22:06:05 +0530

    presigned-put: init commit

---
+++ large-files-upload-directly-s3-put-presigned-uri/src/main/java/com/behl/offloader/LargeFilesUploadDirectlyS3PutPresignedUriApplication.java
@@ -0,0 +1,13 @@
+package com.behl.offloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LargeFilesUploadDirectlyS3PutPresignedUriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LargeFilesUploadDirectlyS3PutPresignedUriApplication.class, args);
+	}
+
+}
