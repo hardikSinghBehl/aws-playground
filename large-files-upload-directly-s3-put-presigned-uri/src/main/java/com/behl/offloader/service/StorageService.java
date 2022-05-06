269536b3a1f5cdc81ccb8dbb5b8274820bb1ce95
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-05-06 22:53:45 +0530

    presigned-put: adding service layer

---
+++ large-files-upload-directly-s3-put-presigned-uri/src/main/java/com/behl/offloader/service/StorageService.java
@@ -0,0 +1,43 @@
+package com.behl.offloader.service;
+
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import org.joda.time.LocalDateTime;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.amazonaws.HttpMethod;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import com.behl.offloader.properties.AwsS3ConfigurationProperties;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@EnableConfigurationProperties(value = AwsS3ConfigurationProperties.class)
+public class StorageService {
+
+	private final AmazonS3 amazonS3;
+	private final AwsS3ConfigurationProperties awsS3ConfigurationProperties;
+
+	public String generateTemporaryPutUri(final String objectKey) {
+		final GeneratePresignedUrlRequest generatePresignedUrlRequest = new GeneratePresignedUrlRequest(
+				awsS3ConfigurationProperties.getS3().getBucketName(), objectKey, HttpMethod.PUT);
+		generatePresignedUrlRequest.setExpiration(new LocalDateTime().plusMinutes(20).toDate());
+
+		final URL presignedUrl = amazonS3.generatePresignedUrl(generatePresignedUrlRequest);
+		try {
+			return presignedUrl.toURI().toString();
+		} catch (final URISyntaxException exception) {
+			log.error("Error occured: {}" + exception);
+			throw new ResponseStatusException(HttpStatus.EXPECTATION_FAILED);
+		}
+	}
+
+}
