391ad97e3c51ec128bf158f20afe383180071682
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-01-04 17:02:19 +0530

    adding API endpoints

---
+++ envelope-encryption/src/main/java/com/behl/cipherinator/controller/UserController.java
@@ -0,0 +1,59 @@
+package com.behl.cipherinator.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.behl.cipherinator.dto.ExceptionResponseDto;
+import com.behl.cipherinator.dto.UserCreationRequestDto;
+import com.behl.cipherinator.dto.UserLoginRequestDto;
+import com.behl.cipherinator.service.UserService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@Tag(name = "User Management", description = "Endpoints for managing user accounts")
+public class UserController {
+
+	private final UserService userService;
+
+	@PostMapping(value = "/api/v1/users", consumes = MediaType.APPLICATION_JSON_VALUE)
+	@Operation(summary = "Creates a user account", description = "Registers a unique user record in the system corresponding to provided information")
+	@ApiResponses(value = { 
+			@ApiResponse(responseCode = "201", description = "User account created successfully",
+					content = @Content(schema = @Schema(implementation = Void.class))),
+			@ApiResponse(responseCode = "409", description = "User account with provided UserName already exists",
+					content = @Content(schema = @Schema(implementation = ExceptionResponseDto.class))),
+			@ApiResponse(responseCode = "400", description = "Malformed request.",
+					content = @Content(schema = @Schema(implementation = ExceptionResponseDto.class)))})
+	public ResponseEntity<HttpStatus> create(@Valid @RequestBody final UserCreationRequestDto userCreationRequest) {
+		userService.create(userCreationRequest);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+	
+	@PostMapping(value = "/api/v1/users/login", consumes = MediaType.APPLICATION_JSON_VALUE)
+	@Operation(summary = "Logs in user into the system", description = "Returns 200 HTTP Response corresponding to valid login credentials")
+	@ApiResponses(value = { 
+			@ApiResponse(responseCode = "200", description = "Authentication successfull",
+					content = @Content(schema = @Schema(implementation = Void.class))),
+			@ApiResponse(responseCode = "401", description = "Bad credentials provided. Failed to authenticate user",
+					content = @Content(schema = @Schema(implementation = ExceptionResponseDto.class))),
+			@ApiResponse(responseCode = "400", description = "Malformed request.",
+					content = @Content(schema = @Schema(implementation = ExceptionResponseDto.class)))})
+	public ResponseEntity<HttpStatus> login(@Valid @RequestBody final UserLoginRequestDto userLoginRequest) {
+		userService.login(userLoginRequest);
+		return ResponseEntity.ok().build();
+	}
+
+}
