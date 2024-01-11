2546e8c275b2b0019ee921e697e09d6297311cc7
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-01-11 16:58:40 +0530

    adding field encryption manager

---
+++ envelope-encryption/src/test/java/com/behl/cipherinator/utility/FieldEncryptionManagerIT.java
@@ -0,0 +1,90 @@
+package com.behl.cipherinator.utility;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.containers.localstack.LocalStackContainer.Service;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.utility.MountableFile;
+
+import com.behl.cipherinator.service.EnvelopeEncryptionService;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import net.bytebuddy.utility.RandomString;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class FieldEncryptionManagerIT {
+	
+	@Autowired
+	private FieldEncryptionManager fieldEncryptionManager;
+	
+	@Autowired
+	private EnvelopeEncryptionService envelopeEncryptionService;
+
+	private static LocalStackContainer localStackContainer;
+
+	static {
+		localStackContainer = new LocalStackContainer(DockerImageName.parse("localstack/localstack:3"))
+				.withCopyFileToContainer(MountableFile.forClasspathResource("init-kms.sh", 0744), "/etc/localstack/init/ready.d/init-kms.sh")
+				.withServices(Service.KMS)
+				.waitingFor(Wait.forLogMessage(".*Executed init-kms.sh.*", 1));
+		localStackContainer.start();
+	}
+
+	@DynamicPropertySource
+	static void properties(DynamicPropertyRegistry registry) {
+		registry.add("com.behl.cipherinator.aws.access-key", localStackContainer::getAccessKey);
+		registry.add("com.behl.cipherinator.aws.secret-access-key", localStackContainer::getSecretKey);
+		registry.add("com.behl.cipherinator.aws.region", localStackContainer::getRegion);
+		registry.add("com.behl.cipherinator.aws.endpoint", localStackContainer::getEndpoint);
+		
+		// Key ID as configured in src/test/resources/init-kms.sh
+		registry.add("com.behl.cipherinator.aws.kms.key-id", () -> "00000000-1111-2222-3333-000000000000");
+	}
+	
+	@Test
+	void shouldEncryptAndDecryptAnnotatedFields() {
+		// prepare object with plaintext values
+		final var plaintextFirstName = RandomString.make();
+		final var plaintextLastName = RandomString.make();
+		final var person = new Person(plaintextFirstName, plaintextLastName);
+		
+		// encrypt the object by calling the method under test 
+		final var encryptor = envelopeEncryptionService.getEncryptor();
+		fieldEncryptionManager.encryptFields(person, encryptor);
+		
+		// assert the field values in the object are encrypted
+		assertThat(person.getFirstName()).isNotBlank().isNotEqualTo(plaintextFirstName).isBase64();
+		assertThat(person.getLastName()).isNotBlank().isNotEqualTo(plaintextLastName).isBase64();
+		
+		// decrypt the object by calling the method under test
+		final var decryptor = envelopeEncryptionService.getDecryptor(encryptor.getEncryptedDataKey());
+		fieldEncryptionManager.decryptFields(person, decryptor);
+		
+		// assert the field values in the object are decrypted back to their original value
+		assertThat(person.getFirstName()).isNotBlank().isEqualTo(plaintextFirstName);
+		assertThat(person.getLastName()).isNotBlank().isEqualTo(plaintextLastName);
+	}
+	
+	@Getter
+	@AllArgsConstructor
+	class Person {
+		
+		@Encryptable
+		private String firstName;
+		
+		@Encryptable
+		private String lastName;
+				
+	}
+	
+}
