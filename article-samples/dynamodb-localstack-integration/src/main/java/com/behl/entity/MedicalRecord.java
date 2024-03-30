59a5b4a075df331cc0e4201f5ff1e1d52e3728cf
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-03-30 15:54:37 +0530

    migrating dynamodb sdk from v1 to v2

--- article-samples/dynamodb-localstack-integration/src/main/java/com/behl/entity/MedicalRecord.java
+++ article-samples/dynamodb-localstack-integration/src/main/java/com/behl/entity/MedicalRecord.java
@@ -1,31 +1,50 @@
 package com.behl.entity;
 
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
-
-import lombok.Getter;
 import lombok.Setter;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
 
-@Getter
 @Setter
-@DynamoDBTable(tableName = "MedicalRecords")
+@DynamoDbBean
 public class MedicalRecord {
 
-	@DynamoDBHashKey
-	@DynamoDBAttribute(attributeName = "Id")
+	public static final String TABLE_NAME = "MedicalRecords";
+
 	private String id;
 
-	@DynamoDBAttribute(attributeName = "PatientName")
 	private String patientName;
 
-	@DynamoDBAttribute(attributeName = "Diagnosis")
 	private String diagnosis;
 
-	@DynamoDBAttribute(attributeName = "TreatmentPlan")
 	private String treatmentPlan;
 
-	@DynamoDBAttribute(attributeName = "AttendingPhysicianName")
 	private String attendingPhysicianName;
+
+	@DynamoDbPartitionKey
+	@DynamoDbAttribute("Id")
+	public String getId() {
+		return id;
+	}
+
+	@DynamoDbAttribute("PatientName")
+	public String getPatientName() {
+		return patientName;
+	}
+
+	@DynamoDbAttribute("Diagnosis")
+	public String getDiagnosis() {
+		return diagnosis;
+	}
+
+	@DynamoDbAttribute("TreatmentPlan")
+	public String getTreatmentPlan() {
+		return treatmentPlan;
+	}
+
+	@DynamoDbAttribute("AttendingPhysicianName")
+	public String getAttendingPhysicianName() {
+		return attendingPhysicianName;
+	}
 
 }
--- article-samples/dynamodb-localstack-integration/src/main/java/com/behl/repository/MedicalRecordRepository.java
+++ article-samples/dynamodb-localstack-integration/src/main/java/com/behl/repository/MedicalRecordRepository.java
@@ -4,24 +4,30 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
 
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.behl.entity.MedicalRecord;
 
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.Key;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
 
 @Repository
-@RequiredArgsConstructor
 public class MedicalRecordRepository {
 
-	private final DynamoDBMapper dynamoDBMapper;
+	private final DynamoDbTable<MedicalRecord> medicalRecordTable;
+
+	public MedicalRecordRepository(DynamoDbEnhancedClient dynamoDbClient) {
+		this.medicalRecordTable = dynamoDbClient.table(MedicalRecord.TABLE_NAME, TableSchema.fromBean(MedicalRecord.class));
+	}
 
 	public void save(@NonNull MedicalRecord medicalRecord) {
-		dynamoDBMapper.save(medicalRecord);
+		medicalRecordTable.putItem(medicalRecord);
 	}
 
 	public Optional<MedicalRecord> findById(@NonNull String medicalRecordId) {
-		var medicalRecord = dynamoDBMapper.load(MedicalRecord.class, medicalRecordId);
+		var key = Key.builder().partitionValue(medicalRecordId).build();
+		var medicalRecord = medicalRecordTable.getItem(key);
 		return Optional.ofNullable(medicalRecord);
 	}
 
