55b5720690f9b173661e8102731181d5b2cf1dae
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-05-14 14:49:51 +0530

    removing article samples

--- article-samples/pii-encryptor-jpa/src/test/resources/init-kms.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-key_id="00000000-1111-2222-3333-000000000000"
-
-awslocal kms create-key --tags '[ { "TagKey" : "_custom_id_" , "TagValue" : "'$key_id'" } ]'
-
-echo "KMS key with keyID '$key_id' created successfully"
-echo "Executed init-kms.sh"
